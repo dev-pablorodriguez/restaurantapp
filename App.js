cc943f43d3da1fa9ed80754780604ade72f2b1e0
Pablo Rodríguez <pablo.rodriguez.musre@gmail.com>
2022-11-11 17:31:56 -0300

    React Navigation y vistas

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
+
+import NuevaOrden from './views/NuevaOrden';
+import Menu from './views/Menu';
+import DetallePlatillo from './views/DetallePlatillo';
+import FormularioPlatillo from './views/FormularioPlatillo';
+import ResumenPedido from './views/ResumenPedido';
+import ProgresoPedido from './views/ProgresoPedido';
+
 const App = () => {
   return (
-    <>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: '#FFDA00'}, headerTitleStyle: { fontWeight: 'bold' } }}>
+        <Stack.Screen name='NuevaOrden' component={ NuevaOrden } options={{ title: 'Nueva Orden' }} />
+        <Stack.Screen name='Menu' component={ Menu } options={{ title: 'Nuestro Menú' }} />
+        <Stack.Screen name='DetallePlatillo' component={ DetallePlatillo } options={{ title: 'Detalle Platillo' }} />
+        <Stack.Screen name='FormularioPlatillo' component={ FormularioPlatillo } options={{ title: 'Formulario Platillo' }} />
+        <Stack.Screen name='ResumenPedido' component={ ResumenPedido } options={{ title: 'Resumen Pedido' }} />
+        <Stack.Screen name='ProgresoPedido' component={ ProgresoPedido } options={{ title: 'Progreso Pedido' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
