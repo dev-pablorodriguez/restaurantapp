fd140f30d17b2f4ee1faa24b2e26090527548723
Pablo Rodríguez <pablo.rodriguez.musre@gmail.com>
2022-11-14 20:47:53 -0300

    Actualizar cantidad y total en pedido si ya existe

--- context/pedidos/pedidosReducer.js
+++ context/pedidos/pedidosReducer.js
@@ -3,7 +3,25 @@ import { ADD_PEDIDO_ITEM } from '../../reducerTypes'
 export default (state, action) => {
     switch(action.type){
         case ADD_PEDIDO_ITEM:
-            return { ...state, pedido: [ ...state.pedido, action.payload ] }
+            let existsInPedido = false;
+
+            //Si el platillo ya se encuentra en la orden del cliente
+            //actualiza la cantidad y el total en lugar de agregarlo nuevamente
+            const pedido = state.pedido.map(platillo => {
+                if(platillo.id === action.payload.id){
+
+                    const { cantidad, total } = action.payload
+
+                    platillo.cantidad += cantidad;
+                    platillo.total += total
+
+                    existsInPedido = true;
+                }
+
+                return platillo;
+            })
+
+            return existsInPedido ? { ...state, pedido } : { ...state, pedido: [ ...state.pedido, action.payload ] }
 
         default:
             return state;
