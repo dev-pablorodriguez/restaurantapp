017dce35944b1f31472bc1234fed016bc82178bc
Pablo Rodríguez <pablo.rodriguez.musre@gmail.com>
2022-11-15 18:26:54 -0300

    Mostrar tiempo de entrega

--- views/ProgresoPedido.js
+++ views/ProgresoPedido.js
@@ -3,6 +3,7 @@ import { View, StyleSheet } from 'react-native'
 import { ListItem, Avatar, Text, Button, Icon } from '@rneui/themed'
 
 import PedidosContext from '../context/pedidos/pedidosContext'
+import firebase from '../firebase'
 import { useNavigation }  from '@react-navigation/native'
 
 import globalStyles from '../styles/global'
@@ -10,8 +11,35 @@ import globalStyles from '../styles/global'
 const ProgresoPedido = ({ route }) => {
   const { idOrden } = route.params;
 
+  const [ tiempoEntrega, setTiempoEntrega ] = useState(0)
+
+  useEffect( () => {
+    const getProducto = () => {
+      firebase.db.collection('ordenes')
+        .doc(idOrden)
+        .onSnapshot(doc => {
+          setTiempoEntrega(doc.data()?.tiempoEntrega ?? 0)
+        })
+    }
+
+    getProducto()
+  }, [])
+
   return (
-    <Text>{ idOrden }</Text>
+    <View style={ globalStyles.contenedor }>
+      <View style={[ globalStyles.contenido, { marginTop: 50 } ]}>
+        { tiempoEntrega === 0 ?
+          <>
+            <Text style={{ textAlign: 'center' }}>¡Hemos recibido tu orden!</Text>
+            <Text style={{ textAlign: 'center' }}>Estamos calculando el tiempo de entrega...</Text>
+          </>
+          :
+          <>
+            <Text style={{ textAlign: 'center' }}>Su orden estará lista en { tiempoEntrega } minutos.</Text>
+          </>
+        }
+      </View>
+    </View>
   )
 }
 
