a5657e1da0011245b743c6964dc7b79cffd49d07
Pablo Rodríguez <pablo.rodriguez.musre@gmail.com>
2022-11-15 22:53:52 -0300

    Implementada cuenta regresiva

--- views/ProgresoPedido.js
+++ views/ProgresoPedido.js
@@ -8,6 +8,8 @@ import { useNavigation }  from '@react-navigation/native'
 
 import globalStyles from '../styles/global'
 
+import Countdown from 'react-countdown'
+
 const ProgresoPedido = ({ route }) => {
   const { idOrden } = route.params;
 
@@ -25,22 +27,54 @@ const ProgresoPedido = ({ route }) => {
     getProducto()
   }, [])
 
+  const getCountdown = ({ minutes, seconds }) =>{
+    let sec = ''
+    if(seconds === 0){
+      sec = `00`
+    }else if(seconds.toString().length === 1){
+      sec = `0${ seconds }`
+    }else{
+      sec = seconds;
+    }
+
+    return `${ minutes }:${ sec }`
+  }
+
+
   return (
     <View style={ globalStyles.contenedor }>
       <View style={[ globalStyles.contenido, { marginTop: 50 } ]}>
         { tiempoEntrega === 0 ?
           <>
-            <Text style={{ textAlign: 'center' }}>¡Hemos recibido tu orden!</Text>
-            <Text style={{ textAlign: 'center' }}>Estamos calculando el tiempo de entrega...</Text>
+            <Text style={ styles.texto }>¡Hemos recibido tu orden!</Text>
+            <Text style={ styles.texto }>Estamos calculando el tiempo de entrega...</Text>
           </>
           :
           <>
-            <Text style={{ textAlign: 'center' }}>Su orden estará lista en { tiempoEntrega } minutos.</Text>
+            <Text style={ styles.texto }>Su orden estará lista en:</Text>
+            <Text style={ styles.countdown }>
+              {/* Convierte los minutos a milisegundos */}
+              <Countdown
+                date={ Date.now() + tiempoEntrega * 60000 }
+                renderer={ getCountdown }
+              />
+            </Text>
           </>
         }
       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  texto: {
+    fontSize: 18,
+    textAlign: 'center'
+  },
+  countdown: {
+    fontSize: 60,
+    textAlign: 'center'
+  }
+})
 
 export default ProgresoPedido
